7e192c014d0c010d0a3bc8ff4f5d57ac59f07e95
ElectroScientist3 <md.rakibul.hasan6@g.bracu.ac.bd>
2025-09-12 13:18:21 +0600

    Update components for v1.1

---
+++ frontend/src/pages/Fitness.jsx
@@ -0,0 +1,9 @@
+function Fitness() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Fitness</h2>
+      {/* Fitness features go here */}
+    </div>
+  );
+}
+export default Fitness;
---
+++ frontend/src/pages/Routine.jsx
@@ -0,0 +1,9 @@
+function Routine() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Routine</h2>
+      {/* Routine features go here */}
+    </div>
+  );
+}
+export default Routine;
